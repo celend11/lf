65bcde2f027800ff2e31ecd3d490a3c353f60a38
liangfu <liangf@telsafe.com.cn>
2015-03-18 13:07:08 +0800

    kkk

---
+++ jjjj/src/com/example/jjjj/Luangd.java
@@ -0,0 +1,32 @@
+package com.example.jjjj;
+
+public class Luangd {
+	private int i;
+	private int b;
+	private int c;
+	private int d;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
+	public int getD() {
+		return d;
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+}
